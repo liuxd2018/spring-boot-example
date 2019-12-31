df292f5cb676d8d152e8e358c0a1dde65e682362
刘晓东 <liuxiaodong@zknext.com>
2019-12-31 13:08:43 +0800

    use in-memory user store to provide security

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan({"com.example"})
-@Import(WebConfig.class)
+@Import({WebConfig.class, SecurityConfig.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("buzz")
+                    .password(passwordEncoder.encode("infinity"))
+                    .authorities("ROLE_USER")
+
+                .and()
+                .withUser("woody")
+                    .password(passwordEncoder.encode("bullseye"))
+                    .authorities("ROLE_USER");
+    }
+}
